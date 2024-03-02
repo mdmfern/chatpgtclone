ce87f679062d4d45ee813d7e80692843360160dc
Devin Fernando <136495746+mdmfern@users.noreply.github.com>
2024-03-02 18:31:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+const chatLog = document.getElementById('chat-log'),
+    userInput = document.getElementById('user-input'),
+    sendButton = document.getElementById('send-button'),
+    buttonIcon = document.getElementById('button-icon'),
+    info = document.querySelector('.info');
+
+sendButton.addEventListener('click', sendMessage);
+userInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+    const message = userInput.value.trim();
+    // if the message is empty = do nothing
+    if (message === '') {
+        return;
+    }
+    // if message = developer - show our message
+    else if (message === 'developer') {
+        // clear input value
+        userInput.value = '';
+        // append message as user - we will code it's function
+        appendMessage('user', message);
+        // sets a fake timeout that showing loading on send button
+        setTimeout(() => {
+            // send our message as a bot(sender : bot)
+            appendMessage('bot', 'This is Coded By Devin Fernando \n https://github.com/mdmfern');
+            // change button icon to default
+            buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+            buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+        }, 2000);
+        return;
+    }
+
+    // else if none of above
+    // appends users message to screen
+    appendMessage('user', message);
+    userInput.value = '';
+
+    const options = {
+        method: 'POST',
+	    headers: {
+            'Content-Type': 'application/json',
+            'X-RapidAPI-Key': 'aff7c87752msh6eca98939db141dp1f3ca5jsn96eb37597138',
+            'X-RapidAPI-Host': 'chat-gpt26.p.rapidapi.com'
+        },
+        body: `{"messages":[{"role":"user","content":"${message}"}]}`
+    };
+    
+    fetch('https://chat-gpt26.p.rapidapi.com/', options).then((response) => response.json()).then((response) => {
+        appendMessage('bot', response.choices[0].message.content);
+
+        buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+        buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+    }).catch((err) => {
+        if (err.name === 'TypeError') {
+            appendMessage('bot', 'Error : Check Your Api Key!');
+            buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+            buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+        }
+    });
+}
+
+function appendMessage(sender, message) {
+    info.style.display = "none";
+    // change send button icon to loading using fontawesome
+    buttonIcon.classList.remove('fa-solid', 'fa-paper-plane');
+    buttonIcon.classList.add('fas', 'fa-spinner', 'fa-pulse');
+
+    const messageElement = document.createElement('div');
+    const iconElement = document.createElement('div');
+    const chatElement = document.createElement('div');
+    const icon = document.createElement('i');
+
+    chatElement.classList.add("chat-box");
+    iconElement.classList.add("icon");
+    messageElement.classList.add(sender);
+    messageElement.innerText = message;
+
+    // add icons depending on who send message bot or user
+    if (sender === 'user') {
+        icon.classList.add('fa-regular', 'fa-user');
+        iconElement.setAttribute('id', 'user-icon');
+    } else {
+        icon.classList.add('fa-solid', 'fa-robot');
+        iconElement.setAttribute('id', 'bot-icon');
+    }
+
+    iconElement.appendChild(icon);
+    chatElement.appendChild(iconElement);
+    chatElement.appendChild(messageElement);
+    chatLog.appendChild(chatElement);
+    chatLog.scrollTo = chatLog.scrollHeight;
+
+}
